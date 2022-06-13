28f577d933c42dc209c5e0aa4dcf6aeca5780390
Laerte <laerteakira@gmail.com>
2022-06-13 20:15:20 -0300

    feat: Set Project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+    return(
+        <BrowserRouter> 
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
