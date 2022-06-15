152cb40c3ae9bc1c5a7eeb18bc3695bb8f539c39
polarisid <danielcarvalhorst@gmail.com>
2022-06-15 20:11:57 -0300

    Feat : AuthContext

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext(null);
+
+const LOCAL_STORAGE_KEY = "linkr-token";
+const persistedToken = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+export function AuthProvider({ children }) {
+	const [token, setToken] = useState(persistedToken);
+
+	function signIn(token) {
+		setToken(token);
+		localStorage.setItem(LOCAL_STORAGE_KEY, token);
+	}
+
+	function signOut() {
+		setToken(null);
+		localStorage.removeItem(LOCAL_STORAGE_KEY);
+	}
+
+	return (
+		<AuthContext.Provider value={{ token, signIn, signOut }}>
+			{children}
+		</AuthContext.Provider>
+	);
+}
