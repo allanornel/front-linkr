f3d7131ae09159e6101bf5dfcacfb15f535af8cf
polarisid <danielcarvalhorst@gmail.com>
2022-06-16 11:20:11 -0300

    Fix:Redirecinando Login se não estiver logado

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,7 +1,17 @@
 import PageContainer from "./../components/PageContainer";
 import CreatePost from "./../components/CreatePost";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 function Timeline() {
+	const { token } = useAuth();
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (!token) {
+			navigate("/");
+		}
+	}, []);
 	return (
 		<>
 			<PageContainer title={"timeline"}>
