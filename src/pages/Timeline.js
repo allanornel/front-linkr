9a99b910e7bc850c520651bab94817db4371b367
Laerte <laerteakira@gmail.com>
2022-06-16 11:04:54 -0300

    feat: applying context on post

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,9 +1,7 @@
 import PageContainer from "./../components/PageContainer";
 import CreatePost from "./../components/CreatePost";
-import useAuth from "../hooks/useAuth";
 
 function Timeline() {
-	const { token } = useAuth();
 	return (
 		<>
 			<PageContainer title={"timeline"}>
