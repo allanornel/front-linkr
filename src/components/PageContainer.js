92d9e1ed75b5e15ad366af13551e361563392488
polarisid <danielcarvalhorst@gmail.com>
2022-06-19 11:47:56 -0300

    Feat: HashtagPage de acordo com a timeline

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useState } from 'react';
+import React from "react";
+import styled from "styled-components";
+import { useState } from "react";
 
-import Header from './Header';
-import PageTitle from './PageTitle';
+import Header from "./Header";
+import PageTitle from "./PageTitle";
 
 function PageContainer({ title, children }) {
-  const [toggle, setToggle] = useState(false);
+	const [toggle, setToggle] = useState(false);
 
-  return (
-    <>
-      <Header toggle={toggle} setToggle={setToggle}/>
-      <Section onClick={() => setToggle(false)}>
-        <PageTitle>{ title }</PageTitle>
-        <Container>{ children }</Container>
-      </Section>
-    </>
-  );
+	return (
+		<>
+			<Header toggle={toggle} setToggle={setToggle} />
+			<Section onClick={() => setToggle(false)}>
+				<PageTitle>{title}</PageTitle>
+				<Container>{children}</Container>
+			</Section>
+		</>
+	);
 }
 
 export default PageContainer;
 
 const Section = styled.section`
-  h4{
-    font-size: 20px;
-    margin-top: 100px;
-    color: #ffffff;
-  }
-  @media (min-width: 620px){
-    margin: 0px auto 0px auto;
-    display: flex;
-    flex-direction: column;
-    width: 620px;
-  }
-`
-
-const Container = styled.div`
-  @media (min-width: 620px){
-    margin: 43px auto 0px auto;
-  }
+	box-sizing: border-box;
+	h4 {
+		font-size: 20px;
+		margin-top: 100px;
+		color: #ffffff;
+	}
+	@media (min-width: 620px) {
+		margin: 0px auto 0px auto;
+		display: flex;
+		flex-direction: column;
+		width: 620px;
+	}
 `;
 
+const Container = styled.div`
+	@media (min-width: 620px) {
+		margin: 43px auto 0px auto;
+	}
+`;
